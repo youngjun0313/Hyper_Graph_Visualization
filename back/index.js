4595df36b489a350ebfa2a4a6288f4849f2a6b8c
youngjun0313 <zerojun0313@kaist.ac.kr>
2021-10-24 06:28:23 +0000

    one hop query for hyperedge

--- back/index.js
+++ back/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'hypergraph_prev',
-    ssl: true
+const mysql = require('sync-mysql');
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'hypergraph_prev',
+//     ssl: true
+// });
+
+const connection = new mysql({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'hypergraph_prev',
+  ssl: true
 });
 
 const simpleVertexColor = "black";
@@ -27,25 +35,20 @@ app.get('/', (req, res) => {
 // vertices(noun vertex)
 app.get('/api/vertices', (req, res) => {
   let vertices = []
-  connection.query('SELECT * FROM vertex', (error, results) => {
-    if (error) 
-      throw error;
+  const results = connection.query("SELECT * FROM vertex");
 
-    results.forEach(element => {
-      vertices.push( {
-        data: { 
-          id: "vertex" + element.vertex_id,
-          label: element.noun_phrase,
-          backgroundColor: simpleVertexColor,
-        },
-        classes: ["noun_vertex"]
-      })
-    });
-
-    console.log(vertices);
-    
-    res.send(vertices)
+  results.forEach(element => {
+    vertices.push( {
+      data: { 
+        id: "vertex" + element.vertex_id,
+        label: element.noun_phrase,
+        backgroundColor: simpleVertexColor,
+      },
+      classes: ["noun_vertex"]
+    })
   });
+    
+  res.send(vertices)
 });
 
 // hyperedges (perdicate vertex)
@@ -53,12 +56,143 @@ app.get('/api/hyperedges', (req, res) => {
   let hyperedges = []
   let pseudo_vertices = []
   let edges = []
-  connection.query('SELECT * FROM hyperedge', (error, results) => {
-    if (error) 
-      throw error;
+  const results = connection.query('SELECT * FROM hyperedge');
 
-    results.forEach(element => {
-      hyperedges.push({
+  results.forEach(element => {
+    hyperedges.push({
+      data: { 
+        id: "hyperedge" + element.hyperedge_id,
+        label: element.predicate_phrase,
+        backgroundColor: predicateColor
+      },
+      classes: ["predicate_vertex"]
+    })
+
+    pseudo_vertices.push({
+      data: {
+        id: "pseudo_vertex" + element.hyperedge_id,
+        label: "",
+        backgroundColor: pseudoVertexColor
+      },
+      classes: ["pseudo_vertex"]
+    })
+
+    edges.push({
+      data: { 
+          id: "hyperedge" + element.hyperedge_id + "->" + "pseudo_vertex" + element.hyperedge_id,
+          source: "hyperedge" + element.hyperedge_id,
+          target: "pseudo_vertex" + element.hyperedge_id
+      },
+      classes: ["arrow_edge"]
+    })
+  });
+  
+  res.send([...hyperedges, ...pseudo_vertices, ...edges])
+});
+
+// edges (noun verex -> hyper edge)
+app.get('/api/edges', (req, res) => {
+  let edges = []
+  const results = connection.query('SELECT vertex_id, hyperedge_id FROM hyperedge_vertex');
+
+  results.forEach(element => {
+    edges.push({
+      data: { 
+          id: "vertex" + element.vertex_id + "->" + "hyperedge" + element.hyperedge_id,
+          source: "vertex" + element.vertex_id,
+          target: "hyperedge" + element.hyperedge_id
+      },
+      classes: ["flat_edge"]
+    })
+  });
+  
+  res.send(edges);
+});
+
+// pseudo_edges (pseudo vertex -> hyperedge)
+app.get('/api/pseudoEdges', (req, res) => {
+  let edges = []
+  const results = connection.query('SELECT pseudovertex_id, hyperedge_id FROM hyperedge_pseudovertex');
+
+  results.forEach(element => {
+    edges.push({
+      data: { 
+          id: "pseudo_vertex" + element.pseudovertex_id + "->" + "hyperedge" + element.hyperedge_id,
+          source: "pseudo_vertex" + element.pseudovertex_id,
+          target: "hyperedge" + element.hyperedge_id
+      },
+      classes: ["flat_edge"]
+    })
+  });
+  
+  res.send(edges);
+});
+
+// Sql input 
+app.get('/api/sqlInput', (req, res) => {
+  const request = req.query;
+
+  let graph = []
+  
+  // input vertex가 pseudo vertex인 경우
+
+  // 먼저 pseudo - pseudo간의 연결 찾기
+
+  // 기준이 되는 node 설정
+  const results = connection.query(`SELECT * FROM hyperedge WHERE predicate_phrase = "${request.vertex}"`);
+
+  const selectedHyperedge = results[0].hyperedge_id;
+
+  console.log("selected hyperedge is : " + selectedHyperedge);
+
+  results.forEach(element => {
+    graph.push({
+      data: { 
+        id: "hyperedge" + element.hyperedge_id,
+        label: element.predicate_phrase,
+        backgroundColor: predicateColor
+      },
+      classes: ["predicate_vertex"]
+    })
+
+    graph.push({
+      data: {
+        id: "pseudo_vertex" + element.hyperedge_id,
+        label: "",
+        backgroundColor: pseudoVertexColor
+      },
+      classes: ["pseudo_vertex"]
+    })
+
+    graph.push({
+      data: { 
+          id: "hyperedge" + element.hyperedge_id + "->" + "pseudo_vertex" + element.hyperedge_id,
+          source: "hyperedge" + element.hyperedge_id,
+          target: "pseudo_vertex" + element.hyperedge_id
+      },
+      classes: ["arrow_edge"]
+    })
+  });
+
+  // hyperedge(predicate vertex)와 연결된 다른 pseudo vertex 찾기 incoming direction
+  const results2 = connection.query(`SELECT pseudovertex_id, hyperedge_id FROM hyperedge_pseudovertex WHERE hyperedge_id = "${selectedHyperedge}"`);
+
+  results2.forEach((element) => {
+    // selected hyperedge와 연결된 pseudo vertex들 합치기
+    graph.push({
+      data: { 
+          id: "pseudo_vertex" + element.pseudovertex_id + "->" + "hyperedge" + element.hyperedge_id,
+          source: "pseudo_vertex" + element.pseudovertex_id,
+          target: "hyperedge" + element.hyperedge_id
+      },
+      classes: ["flat_edge"]
+    })
+
+    const results3 = connection.query(`SELECT * FROM hyperedge WHERE hyperedge_id = "${element.pseudovertex_id}"`);
+
+    // pseudo vertex와 hyperedge 합치기
+    results3.forEach((element) => {
+      graph.push({
         data: { 
           id: "hyperedge" + element.hyperedge_id,
           label: element.predicate_phrase,
@@ -67,7 +201,7 @@ app.get('/api/hyperedges', (req, res) => {
         classes: ["predicate_vertex"]
       })
 
-      pseudo_vertices.push({
+      graph.push({
         data: {
           id: "pseudo_vertex" + element.hyperedge_id,
           label: "",
@@ -76,7 +210,7 @@ app.get('/api/hyperedges', (req, res) => {
         classes: ["pseudo_vertex"]
       })
 
-      edges.push({
+      graph.push({
         data: { 
             id: "hyperedge" + element.hyperedge_id + "->" + "pseudo_vertex" + element.hyperedge_id,
             source: "hyperedge" + element.hyperedge_id,
@@ -85,53 +219,74 @@ app.get('/api/hyperedges', (req, res) => {
         classes: ["arrow_edge"]
       })
     });
-
-    console.log([...hyperedges, ...pseudo_vertices, ...edges]);
-    
-    res.send([...hyperedges, ...pseudo_vertices, ...edges])
   });
-});
 
-// edges
-app.get('/api/edges', (req, res) => {
-  let edges = []
-  connection.query('SELECT vertex_id, hyperedge_id FROM hyperedge_vertex', (error, results) => {
-    if (error) 
-      throw error;
+  // hyperedge(predicate vertex)와 연결된 다른 pseudo vertex 찾기 outgoing direction direction
+  const results4 = connection.query(`SELECT pseudovertex_id, hyperedge_id FROM hyperedge_pseudovertex WHERE pseudovertex_id = "${selectedHyperedge}"`);
 
-    results.forEach(element => {
-      edges.push({
-        data: { 
-            id: "vertex" + element.vertex_id + "->" + "hyperedge" + element.hyperedge_id,
-            source: "vertex" + element.vertex_id,
-            target: "hyperedge" + element.hyperedge_id
-        },
-        classes: ["flat_edge"]
-      })
-    });
-    
-    res.send(edges)
+  results4.forEach((element) => {
+    // selected hyperedge와 연결된 pseudo vertex들 합치기
+    graph.push({
+      data: { 
+          id: "pseudo_vertex" + element.pseudovertex_id + "->" + "hyperedge" + element.hyperedge_id,
+          source: "pseudo_vertex" + element.pseudovertex_id,
+          target: "hyperedge" + element.hyperedge_id
+      },
+      classes: ["flat_edge"]
+    })
+
+    // pseudo vertex와 hyperedge 합치기
+    graph.push({
+      data: { 
+        id: "hyperedge" + element.hyperedge_id,
+        label: element.predicate_phrase,
+        backgroundColor: predicateColor
+      },
+      classes: ["predicate_vertex"]
+    })
+
+    graph.push({
+      data: {
+        id: "pseudo_vertex" + element.hyperedge_id,
+        label: "",
+        backgroundColor: pseudoVertexColor
+      },
+      classes: ["pseudo_vertex"]
+    })
+
+    graph.push({
+      data: { 
+          id: "hyperedge" + element.hyperedge_id + "->" + "pseudo_vertex" + element.hyperedge_id,
+          source: "hyperedge" + element.hyperedge_id,
+          target: "pseudo_vertex" + element.hyperedge_id
+      },
+      classes: ["arrow_edge"]
+    })
   });
-});
 
-// pseudo_edges (pseudo vertex -> hyperedge)
-app.get('/api/pseudoEdges', (req, res) => {
-  let edges = []
-  connection.query('SELECT pseudovertex_id, hyperedge_id FROM hyperedge_pseudovertex', (error, results) => {
-    if (error) 
-      throw error;
-
-    results.forEach(element => {
-      edges.push({
-        data: { 
-            id: "pseudo_vertex" + element.pseudovertex_id + "->" + "hyperedge" + element.hyperedge_id,
-            source: "pseudo_vertex" + element.pseudovertex_id,
-            target: "hyperedge" + element.hyperedge_id
-        },
-        classes: ["flat_edge"]
-      })
-    });
+  // selected hyperedged와 연결된 noun vertex 고르기
+  const results5 = connection.query(`SELECT * FROM hyperedge_vertex NATURAL JOIN vertex WHERE hyperedge_id = "${selectedHyperedge}"`);
     
-    res.send(edges)
+  results5.forEach(element => {
+    graph.push({
+      data: { 
+        id: "vertex" + element.vertex_id,
+        label: element.noun_phrase,
+        backgroundColor: simpleVertexColor,
+      },
+      classes: ["noun_vertex"]
+    })
+    console.log("noun phrsse: " + element.noun_phrase);
+
+    graph.push({
+      data: { 
+          id: "vertex" + element.vertex_id + "->" + "hyperedge" + element.hyperedge_id,
+          source: "vertex" + element.vertex_id,
+          target: "hyperedge" + element.hyperedge_id
+      },
+      classes: ["flat_edge"]
+    })
   });
+
+  res.send(graph);
 });
